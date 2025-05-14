03e27d0d6520e1c06782257cfc951ede1a0ac8af
Sid-CS328 <siddhartharya.cs23@bmsce.ac.in>
2025-05-14 10:36:32 +0530

    Update Floyd.c

--- ADA Lab/Program 7/Floyd.c
+++ ADA Lab/Program 7/Floyd.c
@@ -10,52 +10,58 @@ int min(int, int);
 
 
 int main(){
-printf("Enter the no. of vertices:");
-scanf("%d", &n);
-printf("Enter the cost adjacency matrix:\n");
-int i,j;
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-scanf("%d", &a[i][j]);
-}
-}
-floyd(a,n);
-printf("Distance Matrix:\n");
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-printf("%d ",D[i][j]);
-}
-printf("\n");
-}
-return 0;
+  printf("Enter the no. of vertices:");
+  scanf("%d", &n);
+
+  int i,j;
+  
+  printf("Enter the cost adjacency matrix:\n");
+  for(i=0; i<n; i++){
+    for(j=0; j<n; j++){
+      scanf("%d", &a[i][j]);
+    }
+  }
+  
+  floyd(a,n);
+  
+  printf("Distance Matrix:\n");
+  for(i=0; i<n; i++){
+    for(j=0; j<n; j++){
+      printf("%d ",D[i][j]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
 }
 
 
 
 
 void floyd(int a[][10],int n){
-int i,j,k;
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-D[i][j]=a[i][j];
-}
-} 
-for(k=0;k<n;k++){
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-D[i][j]=min(D[i][j],(D[i][k]+D[k][j]));
-}
-}
-}
+  int i, j, k;
+  
+  for(i=0; i<n; i++){
+    for(j=0; j<n; j++){
+      D[i][j]=a[i][j];
+    }
+  } 
+  for(k=0; k<n; k++){
+    for(i=0; i<n; i++){
+      for(j=0; j<n; j++){
+        D[i][j]=min(D[i][j],(D[i][k]+D[k][j]));
+      }
+    }
+  }
 }
 
 
 
 
 int min(int a,int b){
-if(a<b){
-return a;
-}else{
-return b;
-}
+  if(a<b){
+  return a;
+  }else{
+  return b;
+  }
 }
